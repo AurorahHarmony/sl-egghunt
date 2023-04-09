07c06edd8d8274154155c5659620c1d3ed3ed00b
Aurorah Harmony <aurorahHarmony@gmail.com>
2023-04-09 18:56:45 +1200

    Show timestaps in recent feed

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -30,14 +30,16 @@
             <div class="col-lg-6">
                 <div class="bg-white p-4 rounded-5 shadow mb-3">
                     <h2>Recent</h2>
-
+                    @php
+                        $now = \Carbon\Carbon::now();
+                    @endphp
                     @foreach ($recent_scores as $score)
                     {{-- <a href="/user/{{$score->user_uuid}}" class="text-decoration-none text-dark"> --}}
                         <div class="row align-items-center">
                             {{-- <div class="col-auto">
                                 <img src="https://placehold.co/50x50" class="rounded" height="40" width="40">
                             </div> --}}
-                            <div class="col-auto" style="line-height: 1.25;"><span class="fw-bold">{{$score->score->username}}</span> found an egg in <span class="fw-bold">{{trim(strtok($score->region, '('))}}</span>!<br><span class="fw-light text-muted">{{$score->score->legacy_username}}</span></div>
+                            <div class="col-auto" style="line-height: 1.25;"><span class="fw-bold">{{$score->score->username}}</span> found an egg in <span class="fw-bold">{{trim(strtok($score->region, '('))}}</span>!<br><span class="fw-light text-muted">{{$score->score->legacy_username}} - {{\Carbon\Carbon::createFromTimeString($score->created_at)->diffForHumans($now,true, true, 1)}} ago</span></div>
                             <div class="col text-end">{{$score->score->total_score}}</div>
                         </div>
                         <hr class="my-2">
